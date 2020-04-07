209d5c7c2cef628734fac15994dfeec01deb93e1
Liad Atsmon <liad.atsmon@gmail.com>
2020-04-08 00:04:22 +0300

    check commit

--- src/com/company/Warmup.java
+++ src/com/company/Warmup.java
@@ -22,6 +22,7 @@ public class Warmup {
 
     public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
         int low = 0;
+        int check = 0;
         int high = arr.length;
         int middle = 0;
         while (low < high) {
