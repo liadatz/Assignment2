a2cab8aac9d5bf72386448987b2aec554190929c
Liad Atsmon <liad.atsmon@gmail.com>
2020-04-08 12:01:12 +0300

    2b

--- src/com/company/Warmup.java
+++ src/com/company/Warmup.java
@@ -26,36 +26,24 @@ public class Warmup {
 
     private static int binSearch(int[] arr, int x, Stack myStack, int left, int right) {
         int isCon = isConsistent(arr);
-        if (isCon>0) {
-            int index=0;
+        if (isCon > 0) {
+            int index = 0;
             while (isCon > 0) {
                 index = (int) myStack.pop();
-                isCon = isCon-1;
+                isCon = isCon - 1;
             }
-            if (index < left) {
-                return binSearch(arr,x,myStack,index - (right - index),right);
+            if (index < left) return binSearch(arr, x, myStack, index - (right - index), right);
+            if (index > right) return binSearch(arr, x, myStack, left, index + (index - left));
             }
-            if (index > right) {
-                return binSearch(arr,x,myStack,left,index + (index - left));
-            }
-        }
-
         else {
-            while (left < right) {
+            while (left <= right) {
                 int mid = (left + right) / 2;
                 myStack.push(mid);
-                if (x < arr[mid]) {
-                    return binSearch(arr, x, myStack, left, mid - 1);
-                }
-                if (x > arr[mid]) {
-                    return binSearch(arr, x, myStack, mid + 1, right);
-                }
-                if (x == arr[mid]) {
-                    return mid;
-                }
+                if (x == arr[mid]) return mid;
+                if (x < arr[mid]) return binSearch(arr, x, myStack, left, mid - 1);
+                if (x > arr[mid]) return binSearch(arr, x, myStack, mid + 1, right);
             }
         }
-
         return -1;
     }
 
