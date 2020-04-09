add8b0ff1d8c0eaa364bf40ea772e31b3e8e921f
eden <eden7045@gmail.com>
2020-04-09 15:29:24 +0300

    section 3: exercise 1

--- src/com/company/BacktrackingArray.java
+++ src/com/company/BacktrackingArray.java
@@ -3,64 +3,123 @@ package com.company;
 public class BacktrackingArray implements Array<Integer>, Backtrack {
     private Stack stack;
     private int[] arr;
-    // TODO: implement your code here
+    private int size;
+    private Integer minimum;
+    private Integer maximum;
 
     // Do not change the constructor's signature
     public BacktrackingArray(Stack stack, int size) {
         this.stack = stack;
         arr = new int[size];
+        size = 0;
+        minimum = null;
+        maximum = null;
+
     }
 
     @Override
-    public Integer get(int index){
-        // TODO: implement your code here
-        return null;
+    public Integer get(int index) {
+        return (Integer) arr[index];
     }
 
     @Override
     public Integer search(int x) {
-        // TODO: implement your code here
-        return null;
+        for (int i = 0; i < size; i = i + 1) {
+            if (arr[i] == x)
+                return (Integer) i;
+        }
+        return (Integer) (-1);
     }
 
     @Override
     public void insert(Integer x) {
-        // TODO: implement your code here
+        if (size == arr.length)
+            throw new IllegalArgumentException();
+        stack.push(size);
+        stack.push(minimum);
+        stack.push(maximum);
+        arr[size] = x;
+        if (minimum == null || x < arr[minimum])
+            minimum = (Integer) size;
+        if (maximum == null || x > arr[maximum])
+            maximum = (Integer) size;
+        size = size + 1;
     }
 
     @Override
     public void delete(Integer index) {
-        // TODO: implement your code here
+        stack.push(size);
+        stack.push(minimum);
+        stack.push(maximum);
+        size = size - 1;
+        arr[index] = arr[size];
+        if (size > 0) {
+            if (index == minimum) {
+                minimum = (Integer) 0;
+                for (int i = 1; i < size; i++) {
+                    if (arr[i] < arr[minimum])
+                        minimum = (Integer) i;
+                }
+            }
+            if (index == maximum) {
+                maximum = (Integer) 0;
+                for (int i = 0; i < size; i++) {
+                    if (arr[i] > arr[maximum])
+                        maximum = (Integer) i;
+                }
+            }
+            if (maximum == size)
+                maximum = index;
+            if (minimum == size)
+                minimum = index;
+        } else {
+            maximum = null;
+            minimum = null;
+        }
     }
 
     @Override
     public Integer minimum() {
-        // TODO: implement your code here
-        return null;
+        return minimum;
     }
 
     @Override
     public Integer maximum() {
-        // TODO: implement your code here
-        return null;
+        return maximum;
     }
 
     @Override
     public Integer successor(Integer index) {
-        // TODO: implement your code here
-        return null;
+        Integer output = null;
+        for (int i = 0; i < size; i++)
+            if (arr[i] > arr[index]) {
+                if (output == null || arr[i] < arr[output])
+                    output = (Integer) i;
+            }
+        return output;
     }
 
     @Override
     public Integer predecessor(Integer index) {
-        // TODO: implement your code here
-        return null;
+        Integer output = null;
+        for (int i = 0; i < size; i++)
+            if (arr[i] < arr[index]) {
+                if (output == null || arr[i] > arr[output])
+                    output = (Integer) i;
+            }
+        return output;
     }
 
     @Override
     public void backtrack() {
-        // TODO: implement your code here
+        if (!stack.isEmpty()) {
+            maximum = (Integer) stack.pop();
+            minimum = (Integer) stack.pop();
+            size = (Integer) stack.pop();
+        }
+        System.out.println("backtracking performed");
     }
+
 
     @Override
     public void retrack() {
@@ -69,6 +128,12 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
 
     @Override
     public void print() {
-        // TODO: implement your code here
+        String output = "";
+        for (int i=0; i<size; i++){
+            output = output + arr[i]+ " ";
+        }
+        if (output.length()>0)
+            output.substring(0,output.length()-1);
+        System.out.println(output);
     }
 }
