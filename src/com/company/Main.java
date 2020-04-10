990ac6787b15b43cda7d30e2b2fd352e246b8523
Liad Atsmon <liad.atsmon@gmail.com>
2020-04-11 00:52:55 +0300

    Test's package fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        int[] arr = {1, 1, 2, 14, 15, 16, 23, 99, 100, 100, 100, 132, 193, 196, 197};
-//        Stack myStack = new Stack();
+        int[] arr = {1, 1, 2, 14, 15, 16, 23, 99, 100, 100, 100, 132, 193, 196, 197};
+        Stack myStack = new Stack();
 //        //test 1a
-//        System.out.println(consistentBinSearch(arr,14,myStack));
+        System.out.println(consistentBinSearch(arr,14,myStack));
 //
 //        //test 1b
-//        System.out.println(consistentBinSearch(arr, 13, myStack));
-
-        int check = 1;
-        System.out.println(check++);
-
-
+        System.out.println(consistentBinSearch(arr, 13, myStack));
     }
 }
